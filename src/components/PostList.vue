e5ecca7828907aca50817d42b5bb25a12aa0d4d7
Georgi Valchev <gvalchev@neterra.net>
2021-12-22 22:59:28 +0200

    Listing demo posts

---
+++ src/components/PostList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="post-list">
+    <div v-for="post in posts" :key="post.id">
+      <SinglePost :post="post" />
+    </div>
+  </div>
+</template>
+
+<script>
+import SinglePost from './SinglePost';
+
+export default {
+  name: 'PostList',
+  props: ['posts'],
+  components: {SinglePost}
+};
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,24 @@
 <template>
   <div class="home">
+    <h1>Home</h1>
+    <PostList :posts="posts"/>
   </div>
 </template>
 
 <script>
+import {ref} from 'vue';
+import PostList from '../components/PostList';
+
 export default {
-  name: 'Home'
-}
+  name: 'Home',
+  components: {PostList},
+  setup() {
+    const posts = ref([
+      {id: 1, title: 'Welcome to the blog', body: 'orem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at risus id purus commodo dapibus. Praesent dapibus a purus ut porttitor. Nunc viverra massa ac magna accumsan, in porta urna iaculis. Pellentesque tincidunt non.'},
+      {id: 2, title: 'Top 5 CSS tips', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget libero at diam porttitor luctus at at diam. Phasellus molestie sed quam id dignissim. Morbi a aliquet ipsum. Sed scelerisque nibh sit amet risus laoreet mi.'}
+    ]);
+
+    return {posts};
+  }
+};
 </script>
